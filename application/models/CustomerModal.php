c36e86707dabdce6f139e835c20cecd7bd3fa046
Thilina Rathnayake <thilinarathnayake4@gmail.com>
2021-10-08 00:35:57 +0530

    Customer Login

---
+++ application/models/CustomerModal.php
@@ -0,0 +1,39 @@
+<?php
+
+
+class CustomerModal extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper(array('form', 'url'));
+        $this->load->library(['form_validation', 'session']);
+        $this->load->database();
+    }
+
+    function login($email, $password)
+    {
+        $user = $this->db->get_where('users', ['email' => $email])->row();
+
+        if (!$user) {
+            $this->session->set_flashdata('login_error', 'Please check your email or password and try again.', 300);
+            return false;
+        }
+
+
+        if (!password_verify($password, $user->password)) {
+            $this->session->set_flashdata('login_error', 'Please check your email or password and try again.', 300);
+            return false;
+        }
+
+        $data = array(
+            'user_id' => $user->id,
+            'first_name' => $user->first_name,
+            'last_name' => $user->last_name,
+            'email' => $user->email,
+        );
+
+        $this->session->set_userdata($data);
+        return true;
+    }
+}
---
+++ application/views/customer/customer_login.php
@@ -0,0 +1,48 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Food Zity Login</title>
+    <!-- <link rel="stylesheet" href="style.css"> -->
+    <link href="<?php echo base_url(); ?>assets/styles/css/customer/style.css" rel="stylesheet">
+
+    <!--Fontawesome CDN Link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" />
+</head>
+
+<body>
+    <div class="container">
+        <?php echo form_open('Customer/login'); ?>
+        <div class="form_content">
+            <div class="login_form">
+                <div class="title">Login</div>
+                <div class="input_boxes">
+                    <div class="input_box">
+                        <i class="fas fa-envelope"></i>
+                        <input type="text" name="email" placeholder="Enter your email">
+                    </div>
+                    <?php echo form_error('email'); ?>
+
+                    <div class="input_box">
+                        <i class="fa fa-key"></i>
+                        <input type="password" name="password" placeholder="Enter your password">
+                    </div>
+                    <?php echo form_error('password'); ?>
+
+                    <div class="button input_box">
+                        <input type="submit" name="submit" value="Submit">
+                    </div>
+                    <?php echo $this->session->flashdata('login_error'); ?>
+                    <div class="text">Don't have an account? <label for="">Register Here</label></div>
+                </div>
+            </div>
+        </div>
+
+        <?php form_close(); ?>
+    </div>
+</body>
+
+</html>
